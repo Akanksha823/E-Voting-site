68d905dc9808bbfa6bce1bafcd967530a1e7f7c8
Akanksha823 <vermaakanksha823@gmail.com>
2022-05-16 17:07:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+const port = 3000;
+
+// app.use(express.static(__dirname,'./public')) //middleware
+app.use(express.static(__dirname + '/public'))
+console.log(__dirname)
+app.get('/', (req, res) => {
+    res.send("hello about page")
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`aavaj aa rhi h ? ${port}`)
+})  
